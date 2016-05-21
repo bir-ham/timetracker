23b199a8b93a3d7c70be11a5a336735086fdba26
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-05-21 14:44:57 +0300

    Comment out Confirmable section. Add confirmable devise filter.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  # :lockable, :timeoutable and :omniauthable
+  devise :invitable, :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   validates :first_name, presence: true
   validates :second_name, presence: true
