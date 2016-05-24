0fcc6cebd7a8a79fcf6316b4a4921ae62d0f43f7
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-05-24 16:41:11 +0300

    Add numericality validation.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ActiveRecord::Base
   validates :name, presence: true
-  validates :phone_number, presence: true, uniqueness: true, allow_nil: true
+  validates :phone_number, presence: true, uniqueness: true, allow_nil: true,
+    numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 15, message: 'Phone nubmer 
+    should be between 10 and 15' }
   validates :email, presence: true, uniqueness: true, allow_nil: true
   validates :address, length: { maximum: 100, 
       too_long: "%{count} characters is the maximum allowed"
@@ -9,7 +11,7 @@ class Customer < ActiveRecord::Base
 
   private
   def choose_xand_contact
-    unless phone_number.blank? ^ email.blank?
+    if phone_number.blank? ^ email.blank?
       errors.add(:base, 'add a phone number or an email. Not both empty')        
     end
   end  
