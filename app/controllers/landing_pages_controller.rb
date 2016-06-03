8c2921839c64af8f620ca10351ee49fa4c7a6c2e
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-06-03 13:24:00 +0300

    Move homepages controller to dashboard. Add landing_pages controller & view resources. Remove homepage view resources

---
+++ app/controllers/landing_pages_controller.rb
@@ -0,0 +1,23 @@
+class LandingPagesController < ApplicationController
+  
+  def index
+        
+  end
+  
+  def about
+
+  end
+  
+  def contact 
+
+  end
+
+  def faq
+
+  end
+
+  def pricing
+
+  end
+    
+end
