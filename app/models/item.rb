e3111734bd4dc0cd1a2e307f3d804aa55d13c9c9
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-06-08 23:21:47 +0300

    Write modle spec. Add validation fields. Add item route resource.

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,11 @@
 class Item < ActiveRecord::Base
   belongs_to :invoice
+
+  validates :description, presence: true
+  validates :date, presence: true
+  validates :quantity, presence: true
+  validates :unit, presence: true
+  validates :unit_price, presence: true
+  validates :vat, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: 'Vat
+   percentage should be between 0 and 100' }, allow_nil: true 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
     resources :invoices 
     resources :invoice_imports 
     resources :customers
+    resources :items
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
