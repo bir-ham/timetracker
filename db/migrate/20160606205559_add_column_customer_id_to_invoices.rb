fc159b1da096ff1aa5e561a3d92cd745ac7566dc
bir-ham <birhanuh@gmail.com>
2016-06-09 15:00:54 +0300

    Remove remove customer_id migraton sinppet. Write item-create, item-edit and item-delete spec.

--- db/migrate/20160606205559_add_column_customer_id_to_invoices.rb
+++ db/migrate/20160606205559_add_column_customer_id_to_invoices.rb
@@ -1,7 +1,6 @@
 class AddColumnCustomerIdToInvoices < ActiveRecord::Migration
   def change
     remove_column :invoices, :customer, :string
-    remove_column :invoices, :customer_id, :integer
     add_column :invoices, :customer_id, :integer
   end
 end
