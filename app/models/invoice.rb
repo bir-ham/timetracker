f1d2ea31fb90ed77bd8a8f1760c6aa5e9ec604fb
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-07-06 17:55:18 +0300

    Move lamda method to a method.

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,26 +1,26 @@
 class Invoice < ActiveRecord::Base
   include ActiveModel::Validations
-  attr_writer :current_step
+  attr_accessor :current_step
 
   belongs_to :customer
   belongs_to :user
   has_many :items, dependent: :destroy
 
-  validates :customer, presence: true, :if => lambda { |o| o.current_step == 'customer' }
-  validates :user, presence: true, if: lambda { |o| o.current_step == 'invoice' }
-  validates :date_of_an_invoice, presence: true, if: lambda { |o| o.current_step == 'invoice' }
+  validates :customer, presence: true, if: :customer? 
+  validates :user, presence: true, if: :invoice?
+  validates :date_of_an_invoice, presence: true, if: :invoice?
   validates :deadline, presence: true, allow_nil: true
   validates :payment_term, presence: true, allow_nil: true
   validates :interest_in_arrears, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 100, message: 'Interest on arrears
-    percentage should be between 0 and 100' }, allow_nil: true, if: lambda { |o| o.current_step == 'invoice' }
+    percentage should be between 0 and 100' }, allow_nil: true, if: :invoice?
   validates :reference_number, presence: true,
     uniqueness: true,
-    numericality: { greater_than_or_equal_to: 0 }, if: lambda { |o| o.current_step == 'invoice' }
+    numericality: { greater_than_or_equal_to: 0 }, if: :invoice?
   validates :description, length: { maximum: 300,
-    too_long: "%{count} characters is the maximum allowed" }, if: lambda { |o| o.current_step == 'invoice' }
+    too_long: "%{count} characters is the maximum allowed" }, if: :invoice?
 
-  validate :choose_xor_deadline_payment_term, if: lambda { |o| o.current_step == 'invoice' }
-  validate :date_of_an_invoice_or_deadline_cannot_be_in_the_past, if: lambda { |o| o.current_step == 'invoice' }
+  validate :choose_xor_deadline_payment_term, if: :invoice?
+  validate :date_of_an_invoice_or_deadline_cannot_be_in_the_past, if: :invoice?
 
   def self.to_csv(options = {})
     CSV.generate(options) do |csv|
@@ -31,6 +31,15 @@ class Invoice < ActiveRecord::Base
     end
   end
 
+  def customer?
+    lambda { |o| o.current_step == 'customer' }  
+  end
+
+  def invoice?
+    lambda { |o| o.current_step == 'invoice' }  
+  end
+
+  # setter
   def current_step
     @current_step || steps.first
   end
