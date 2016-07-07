cd0c601566646a34497343af71c21c49059a2ea6
Birhanu Hailemariam <birhanu.hailemariam@gmail.com>
2016-07-07 19:18:18 +0300

    Add text-vertical class. Move invoice-step fields under context block.

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -3,22 +3,27 @@ require 'rails_helper'
 RSpec.describe Invoice, type: :model do
   describe 'validations' do
     it { should validate_presence_of :customer }
-    it { should validate_presence_of :user }
-    it { should validate_presence_of :date_of_an_invoice }
-    it { should validate_presence_of :reference_number }
-    it { should validate_uniqueness_of :reference_number }
-    
-    it { should allow_value('', nil).for(:interest_in_arrears) }
-    it { should allow_value('', nil).for(:description) }
-    it { should allow_value('1').for(:interest_in_arrears) }
-    it { should_not allow_value('101').for(:interest_in_arrears) }
-    it { should allow_value('lorem').for(:description) }
 
-    it 'should validate deadline or payment term' do 
-      expect(build(:invoice, deadline: Date.current.tomorrow, payment_term: '')).to be_valid
-      expect(build(:invoice, deadline: '', payment_term: '2')).to be_valid
-      expect(build(:invoice, deadline: Date.current.tomorrow, payment_term: '2')).to be_invalid
-    end
+    context 'if invoice step' do 
+      before { subject { lambda { |i| i.current_step == 'invoice' } } }
+
+      it { should validate_presence_of :user }
+      it { should validate_presence_of :date_of_an_invoice }
+      it { should validate_presence_of :reference_number }
+      it { should validate_uniqueness_of :reference_number }
+      
+      it { should allow_value('', nil).for(:interest_in_arrears) }
+      it { should allow_value('', nil).for(:description) }
+      it { should allow_value('1').for(:interest_in_arrears) }
+      it { should_not allow_value('101').for(:interest_in_arrears) }
+      it { should allow_value('lorem').for(:description) }
+
+      it 'should validate deadline or payment term' do 
+        expect(build(:invoice, deadline: Date.current.tomorrow, payment_term: '')).to be_valid
+        expect(build(:invoice, deadline: '', payment_term: '2')).to be_valid
+        expect(build(:invoice, deadline: Date.current.tomorrow, payment_term: '2')).to be_invalid
+      end
+    end  
   end
 
   describe 'associations' do
