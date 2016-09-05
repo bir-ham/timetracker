6afad5ca339cfba6b546af05f7be3b0a21a6a2b9
bir-ham <birhanuh@gmail.com>
2016-09-05 21:39:41 +0300

    Store Add_new_customer button called-from url to session.

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,6 +1,6 @@
-class CustomersController < ApplicationController 
+class CustomersController < ApplicationController
   #before_action :set_invoice, only: [:show, :edit, :upate, :destroy]
-  
+
   def index
     @customers = Customer.all
     #@search = InvoiceSearch.new(params[:search])
@@ -9,51 +9,56 @@ class CustomersController < ApplicationController
 
   def new
     @customer = Customer.new
+    session[:new_action_called_from] = URI(request.referer).path
   end
 
   def create
     @customer = Customer.new(customer_params)
     if @customer.save
-      redirect_to customers_path, notice: I18n.t('customers.create.notice_create')
+      if session[:new_action_called_from] == '/projects/new'
+        redirect_to new_project_path
+      else
+        redirect_to customers_path, notice: I18n.t('customers.create.notice_create')
+      end
     else
       render :new
-    end  
+    end
   end
 
   # GET /invoices/1
-  def show 
-    @customer = Customer.find(params[:id]) 
+  def show
+    @customer = Customer.find(params[:id])
   end
 
   # GET /invoices/1/edit
   def edit
-    @customer = Customer.find(params[:id]) 
+    @customer = Customer.find(params[:id])
   end
 
   def update
-    @customer = Customer.find(params[:id]) 
+    @customer = Customer.find(params[:id])
     if @customer.update_attributes(customer_params)
       flash.now[:success] = I18n.t('customers.update.success_update')
       render :show
     else
       render :edit
-    end 
+    end
   end
 
   def destroy
-    @customer = Customer.find(params[:id]) 
+    @customer = Customer.find(params[:id])
     if @customer.destroy
       redirect_to customers_url, notice: I18n.t('customers.destroy.success_delete')
     else
       flash.now[:error] = I18n.t('invoices.distroy.error_delete')
-    end  
+    end
   end
 
   private
 
   def customer_params
-    params.require(:customer).permit(:name, :phone_number, :email, 
+    params.require(:customer).permit(:name, :phone_number, :email,
       :address)
-  end  
+  end
 
 end
