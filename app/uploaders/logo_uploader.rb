992037d48b867e40318c029881974181e47e545a
bir-ham <birhanuh@gmail.com>
2016-09-24 12:54:44 +0300

    Modify dimentions of thumb & normal versions. Add logo-placeholder-normal.png

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -33,13 +33,13 @@ class LogoUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [100, 70]
+    process :resize_to_fit => [70, 70]
   end
 
   version :normal do
-    process :resize_to_fit => [150, 105]
+    process :resize_to_fit => [120, 120]
   end
-  
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
