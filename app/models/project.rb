e0e5b9df46fec39d675109b1fe8833f0ccb7204e
bir-ham <birhanuh@gmail.com>
2016-09-24 19:52:00 +0300

    Add checkbox_and_radios warpper to archived checkbox.

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,7 +7,7 @@ class Project < ActiveRecord::Base
   validates :customer, presence: true
   validates :user, presence: true
   validates :name, presence: true, uniqueness: true
-  validates :deadline, presence: true, allow_nil: true
+  validates :deadline, presence: false
   validates :status, presence: true
   validates :progress, presence: true, allow_nil: true
   validates :description, presence: false
