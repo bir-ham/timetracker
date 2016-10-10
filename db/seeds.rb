c04a757df3ed25a262e32956259f760de6061098
bir-ham <birhanuh@gmail.com>
2016-10-11 00:48:42 +0300

    Add tasks and items seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,13 +25,27 @@ projects = Project.create([{ name: 'Project1', deadline: Date.today, user: user,
   {name: 'Project4', deadline: Date.today, user: user, customer: customers[0], status: 'DELAYED', description: 'Project4 description' },
   {name: 'Project5', deadline: Date.today, user: user, customer: customers[0], status: 'ONGOING', description: 'Project5 description' }])
 
+tasks = Task.create([{ name: 'Design', hours: '2:15', payment_type: 'Per task', price: 5, vat: 15, project: projects[0] },
+  { name: 'Typing', hours: '3:00', payment_type: 'Per task', price: 15, vat: 15, project: projects[1] },
+  { name: 'Translation', hours: '1:00', payment_type: 'Per task', price: 2.50, vat: nil, project: projects[2] },
+  { name: 'Typing', hours: '4:50', payment_type: 'Per hour', price: 5, vat: 15, project: projects[4] },
+  { name: 'Printing', hours: '3:15', payment_type: 'Per hour', price: 5, vat: 15, project: projects[5] },
+  { name: 'Binding', hours: '4:15', payment_type: 'Per hour', price: 3.5, vat: 15, project: projects[6] }])
+
 sales = Sale.create([{ date: Date.today, user: user, customer: customers[0], status: 'DELIVERED', description: 'Sale description' },
   { date: Date.today, user: user, customer: customers[0], status: 'PREPARING', description: 'Sale description' },
   { date: Date.today, user: user, customer: customers[1], status: 'PREPARING', description: 'Sale description' },
-  { date: Date.today, user: user, customer: customers[3], status: 'NEW', description: 'Sale description' },
-  { date: Date.today, user: user, customer: customers[4], status: 'NEW', description: 'Sale description' },
-  { date: Date.today, user: user, customer: customers[0], status: 'WAITING', description: 'Sale description' },
+  { date: Date.today, user: user, customer: customers[3], status: 'DELIVERED', description: 'Sale description' },
+  { date: Date.today, user: user, customer: customers[4], status: 'DELIVERED', description: 'Sale description' },
+  { date: Date.today, user: user, customer: customers[2], status: 'WAITING', description: 'Sale description' },
   { date: Date.today, user: user, customer: customers[0], status: 'WAITING', description: 'Sale description' }])
+
+items = Item.create([{ name: 'Pencil', quantity: 2, unit: 'Piece', unit_price: 5, vat: 15, sale: sales[0] },
+  { name: 'Book', quantity: 2, unit: 'Piece', unit_price: 15, vat: 15, sale: sales[1] },
+  { name: 'Ruler', quantity: 1, unit: 'Piece', unit_price: 2.50, vat: nil, sale: sales[2] },
+  { name: 'Sugar', quantity: 4, unit: 'Kilo gram', unit_price: 5, vat: 15, sale: sales[3] },
+  { name: 'Wheate', quantity: 3, unit: 'Kilo gram', unit_price: 5, vat: 15, sale: sales[4] },
+  { name: 'Sesame', quantity: 4, unit: 'Kilo gram', unit_price: 3.5, vat: 15, sale: sales[5] }])
 
 invoices = Invoice.create([{ user: user, sale: sales[0], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54321', description: 'Sale description' },
   { user: user, sale: sales[1], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54322', description: 'Sale description' },
@@ -43,4 +57,5 @@ invoices = Invoice.create([{ user: user, sale: sales[0], date_of_an_invoice: Dat
   { user: user, project: projects[4], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54322', description: 'Sale description' },
   { user: user, project: projects[5], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54322', description: 'Sale description' },
   { user: user, project: projects[6], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54322', description: 'Sale description' },
+  { user: user, sale: sales[4], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54323', description: 'Sale description' },
   { user: user, sale: sales[5], date_of_an_invoice: Date.today, deadline: Date.tomorrow, reference_number: '54323', description: 'Sale description' }])
