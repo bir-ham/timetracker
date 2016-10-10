1808cc1b0de4696bba98dae981447e02e3812017
bir-ham <birhanuh@gmail.com>
2016-10-10 18:17:45 +0300

    Change it block text.

--- spec/features/account_creation_feature_spec.rb
+++ spec/features/account_creation_feature_spec.rb
@@ -4,7 +4,7 @@ describe 'account creation' do
   let(:subdomain) { generate(:subdomain) }
   before(:each) { sign_up(subdomain) }
 
-  it 'confirmation message' do
+  it 'shows confirmation message' do
     expect(page).to have_text I18n.t('devise.registrations.signed_up_but_unconfirmed')
   end
 
@@ -13,9 +13,6 @@ describe 'account creation' do
       open_email 'birhanu@example.com'
       expect(current_email).to have_body_text("You can confirm your account email through the link below:")
       visit_in_email 'Confirm my account'
-    end
-
-    it "shows confirmation message" do
       expect(page).to have_content('Your account was successfully confirmed')
     end
 
@@ -53,18 +50,17 @@ describe 'account creation' do
     visit root_url(subdomain: false)
     click_link I18n.t('button.sign_up')
 
-    reset_mailer
-
     #binding.pry #inserts a breakpoint here
     fill_in 'First name', with: 'Birhanu'
     fill_in 'Last name', with: 'Hailemariam'
-    fill_in 'Email', with: 'birhanu@example.com'
+    fill_in 'Email', with: 'birhanuh@gmail.com'
     within('.account_owner_password') do
       fill_in 'Password', with: 'pw'
     end
     fill_in 'Password confirmation', with: 'pw'
     fill_in 'Subdomain', with: subdomain
     click_button 'Create Account'
+
   end
 end
 
