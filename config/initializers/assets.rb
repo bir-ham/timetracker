dff8452a7093072a86749fed151803a89234bd74
Birhanu Hailemariam <birhanuh@gmail.com>
2016-11-06 15:40:31 +0200

    Add fonts folder with OpenSans font file. Call @font-face to call source OpenSans fonts. Include fonts folder to asstes config. Move javascripts and stylesheets public assets paths to initializers/assets.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,6 +3,10 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
+# Configure javascripts and stylesheets public assets to assets paths 
+Rails.application.config.assets.paths << Rails.root.join('public', 'assets', 'javascripts')
+Rails.application.config.assets.paths << Rails.root.join('public', 'assets', 'stylesheets')
+
 Rails.application.config.assets.precompile += %w( landing_page.css )
 Rails.application.config.assets.precompile += %w( charts.js )
 Rails.application.config.assets.precompile += %w( date_time_picker.js )
