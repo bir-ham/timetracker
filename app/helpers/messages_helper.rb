af287549aa1a3fa1ad18db1a3f0c01e77760fac1
Birhanu Hailemariam <birhanuh@gmail.com>
2016-11-11 23:09:58 +0200

    Add chose_recipient instance varaible. Call order on User. Add send message link.

--- app/helpers/messages_helper.rb
+++ app/helpers/messages_helper.rb
@@ -1,11 +1,11 @@
 module MessagesHelper
   
-  def recipients_options
+  def recipients_options(chose_recipient = nil)
     avatar = if participant[:avatar] ? participant.avatar.small : 'avatar-placeholder-small'
       
     s = ''
     User.all.each do |user|
-      s << "<option value='#{user.id}' data-img-src='#{avatar}'>#{user.first_name}</option>"
+      s << "<option value='#{user.id}' data-img-src='#{avatar} #{'selected' if user = chose_recipient }'>#{user.first_name}</option>"
     end
     s.html_safe  
   end
