9a25ee54358504de89cef04ecc584a99bfcc8214
Birhanu Hailemariam <birhanuh@gmail.com>
2016-11-14 23:43:12 +0200

    Add conversations_feature_spec & dashboard_feature_spec specs.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,7 +7,7 @@ class MessagesController < ApplicationController
   def create
     recipients = User.where(id: params['recipients'])
     conversation = current_user.send_message(recipients, params[:message][:body], params[:message][:subject]).conversation
-    flash[:success] = 'Message has been sent!'
+    flash[:success] = I18n.t('messages.create.success')
     redirect_to conversations_path
   end
 
---
+++ spec/features/conversations_feature_spec.rb
@@ -0,0 +1,47 @@
+require 'rails_helper'
+
+describe 'conversations' do
+  let!(:account) { create(:account_with_schema) }
+  let(:user) { account.owner }
+
+  before do
+    set_subdomain(account.subdomain)
+    sign_user_in(user)
+  end
+
+  it 'allows user to send email' do
+    click_link I18n.t('navigation.see_all_messages'
+
+    expect(page).to have_text I18n.t('conversations.index.inbox')
+    expect(page).to have_link I18n.t('conversations.index.new_message')
+
+    click_link I18n.t('conversations.index.new_message')
+    expect(page).to have_text I18n.t('messages.new.header')
+
+    within('.chosen-it') do
+      select_generic(user.first_name)
+    end
+    fill_in 'message[subject]', with: 'Test title'
+    fill_in 'message[body]', with: 'Lorem lipsum'
+
+    submit_form_button
+
+    expect(page).to have_text I18n.t('messages.create.success')
+    within('.badge') do
+      expect(page).to have_text '1'
+    end
+  end
+
+  it 'allows user to read sent email' do
+    click_link I18n.t('navigation.see_all_messages'
+
+    expect(page).to have_text I18n.t('conversations.index.sent')
+
+    find(:xpath, "//a[@href='/conversations?box=sent']").click
+    expect(page).to have_link 'Test title'
+
+    click_link 'Test title'
+
+    expect(page).to have_text 'Lorem lipsum'
+  end
+end
