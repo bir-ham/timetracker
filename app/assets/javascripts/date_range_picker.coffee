d71889620d4e562025e23968e16554188c5fc128
Birhanu Hailemariam <birhanuh@gmail.com>
2016-11-21 14:34:54 +0200

    Change calendar, customer_collapse and date_range_picker to coffee script.

---
+++ app/assets/javascripts/date_range_picker.coffee
@@ -0,0 +1,12 @@
+date_range_picker = undefined
+date_range_picker = ->
+  $('.date-range-picker').each ->
+    $(this).daterangepicker {
+      timePicker: true
+      timePickerIncrement: 30
+      alwaysShowCalendars: true
+    }, (start, end, label) ->
+      $('.start_hidden').val start.format('YYYY-MM-DD HH:mm')
+      $('.end_hidden').val end.format('YYYY-MM-DD HH:mm')
+       
+$(document).on 'ready', date_range_picker
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,8 +2,8 @@ class Invoice < ActiveRecord::Base
   attr_accessor :current_step
 
   belongs_to  :user
-  belongs_to :sale
-  belongs_to :project
+  belongs_to :sale, dependent: :destroy
+  belongs_to :project, dependent: :destroy
 
   validates :sale, presence: true, allow_nil: true, if: lambda { |i| i.current_step == 'sale_project' }
   validates :project, presence: true, allow_nil: true, if: lambda { |i| i.current_step == 'sale_project' }
