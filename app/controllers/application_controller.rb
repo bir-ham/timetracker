1fa2787202770d00aaa508107a97f9498aed6461
Birhanu Hailemariam <birhanuh@gmail.com>
2016-11-26 23:11:35 +0200

    Add :current_password field.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,8 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:accept_invitation, keys: [:first_name, :last_name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, 
+        :current_password, :first_name, :last_name])
   end
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,7 @@ class User < ActiveRecord::Base
   
   has_many :invoices
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+
 
   mount_uploader :avatar, Uploader
 
