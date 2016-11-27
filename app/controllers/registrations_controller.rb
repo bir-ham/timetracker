ed2f48db89c95bb12f257c07ef8d7c9f37c32e38
Birhanu Hailemariam <birhanuh@gmail.com>
2016-11-27 22:20:59 +0200

    Add model validation. Chagne create actions logic. Store param values in a session and pass as input_html.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,13 +2,17 @@ class RegistrationsController < Devise::RegistrationsController
   
   protected
 
-  def update_resource(resource, params)
-    params.delete :current_password
-    resource.update_without_password(params)
+  def update_resource(resource, user_params)
+    user_params.delete :current_password
+    resource.update_without_password(user_params)
   end
 
   def after_update_path_for(resource)
     current_account
+  end
+
+  def user_params 
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
   end
 
 end
